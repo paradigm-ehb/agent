209caa0b9bb29186c29a59c396826c202575ae02
nasr <nsrddyn@gmail.com>
2025-11-27 22:59:08 +0100

    feature: improved the build script for installing the proto buf go plugins and the protocol buffer compiler

--- build.sh
+++ build.sh
@@ -1,7 +1,33 @@
 #!/bin/sh
 
+echo -------------------------------------------------------------------------
+echo Description: Script to download protoc compiler and compile protoc files
+echo -------------------------------------------------------------------------
+
+# Install protocol buffer compiler
+# source : https://protobuf.dev/installation/
+PB_REL="https://github.com/protocolbuffers/protobuf/releases"
+curl -LO $PB_REL/download/v30.2/protoc-30.2-linux-x86_64.zip
+unzip protoc-30.2-linux-x86_64.zip -d $HOME/.local
+export PATH="$PATH:$HOME/.local/bin"
+
+# Install protoc compiler plugins for Go
+go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+
+# Add protoc compiler to binary
+export PATH="$PATH:$(go env GOPATH)/bin"
+
+# compile protoc files
 srcdir="."
 outdir="internal/connection/pb"
 
-protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative "$srcdir/internal/connection/greet.proto"
+if ls $srcdir/internal/connection/*proto; then
+    protoc --go_out=. --go_opt=paths=source_relative \
+        --go-grpc_out=. --go-grpc_opt=paths=source_relative \
+        $srcdir/internal/connection/*.proto
+else
+    echo "Geen proto file gevonden :("
+    exit 1
+fi
 
