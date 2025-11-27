e0a3c587f0560a32899656755289c198a231bfcf
nasr-server <nsrddyn@gmail.com>
2025-11-27 11:55:34 +0100

    debug: small refactorings to connect with remote

--- cmd/main.go
+++ cmd/main.go
@@ -30,7 +30,7 @@ func (s *server) SayHello(_ context.Context, in *pb.HelloRequest) (*pb.HelloRepl
 
 func main() {
 	flag.Parse()
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	lis, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%d", *port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
