8f54327b401a224ee57c2b939cd6c219e5302aec
nasr <nsrddyn@gmail.com>
2025-12-03 22:20:24 +0100

    feature: operating system check

--- cmd/agent/agent.go
+++ cmd/agent/agent.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"os"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
@@ -13,6 +14,7 @@ import (
 	"paradigm-ehb/agent/pkg/service"
 
 	manager "paradigm-ehb/agent/internal/svcmanager"
+	tools "paradigm-ehb/agent/tools"
 )
 
 var (
@@ -20,6 +22,24 @@ var (
 )
 
 func main() {
+	fmt.Println(`
+                               .___.__                                                       __   
+___________ ____________     __| _/|__| ____   _____           _____     ____   ____   _____/  |_ 
+\____ \__  \\_  __ \__  \   / __ | |  |/ ___\ /     \   ______ \__  \   / ___\_/ __ \ /    \   __\
+|  |_> > __ \|  | \// __ \_/ /_/ | |  / /_/  >  Y Y  \ /_____/  / __ \_/ /_/  >  ___/|   |  \  |  
+|   __(____  /__|  (____  /\____ | |__\___  /|__|_|  /         (____  /\___  / \___  >___|  /__|  
+|__|       \/           \/      \/   /_____/       \/               \//_____/      \/     \/      
+
+
+	`)
+
+	err := tools.CheckOSUser()
+	if err != nil {
+		fmt.Println("Operating system is currently not supported. Come back in .... never! Imagine not using Linux. Not worthy.")
+		os.Exit(4)
+	}
+
+	// TODO: replace with actual services
 	fmt.Println(manager.ListDbusObject())
 
 	flag.Parse()
