149f7a50692f3fc9c633665d9db0b73fe5f591f5
nasr <nsrddyn@gmail.com>
2025-12-16 22:32:18 +0100

    refactor: updated grpcurl command

--- test/grpctest.sh
+++ test/grpctest.sh
@@ -1,4 +1,10 @@
-echo 'export PATH="$HOME/go/bin:$PATH"' >> ~/.bashrc
-source ~/.bashrc
-
-grpcurl -plaintext -d '{ "time": "1m", "numFromTail": 10, "cursor": "", "field": "Systemd", "value": "ssh.service", "path": "" }' localhost:50051  journal.v1.JournalService/Action
+grpcurl -plaintext \
+  -d '{
+    "time": "",
+    "numFromTail": 0,
+    "cursor": "",
+    "field": "GID",
+	"value": "1000",
+    "path": ""
+  }' \
+  localhost:50051 journal.v1.JournalService/Action
