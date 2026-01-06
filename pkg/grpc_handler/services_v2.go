d892abe7b043b6595f38ddc26e7869d1def40db0
nasr <nsrddyn@gmail.com>
2026-01-06 11:22:22 +0100

    feature: grpc handler for v2 services

--- pkg/grpc_handler/services_v2.go
+++ pkg/grpc_handler/services_v2.go
@@ -3,100 +3,75 @@ package grpc_handler
 import (
 	"context"
 	"log"
-
 	manager "paradigm-ehb/agent/internal/dbusservices"
-
-	"paradigm-ehb/agent/gen/services/v1"
+	"paradigm-ehb/agent/gen/services/v2"
 	dh "paradigm-ehb/agent/internal/dbusservices/dbus"
 	"paradigm-ehb/agent/internal/dbusservices/systemd"
-
 	"github.com/godbus/dbus"
 )
 
-type HandlerService struct {
-	services.UnimplementedHandlerServiceServer
+type HandlerServiceV2 struct {
+	v2.UnimplementedHandlerServiceServer
 }
 
-func (s *HandlerService) UnitAction(_ context.Context, in *services.ServiceActionRequest) (*services.ServiceActionReply, error) {
-
-	var out string
+func (s *HandlerService) UnitActionV2(_ context.Context, in *v2.ServiceActionRequest) (*v2.ServiceActionReply, error) {
+	var out []byte
 	conn, err := dh.CreateSystemBus()
 	if err != nil {
-
 		log.Println("failed to create systembus")
-		out = "failed system bus"
+		out = []byte("failed system bus")
 	}
-
 	defer func(conn *dbus.Conn) {
 		err := conn.Close()
 		if err != nil {
-
 		}
 	}(conn)
 
-	if in.GetUnitFileAction() == services.ServiceActionRequest_UNIT_FILE_ACTION_ENABLE {
-
+	if in.GetUnitFileAction() == v2.ServiceActionRequest_UNIT_FILE_ACTION_ENABLE {
 		err = manager.RunSymlinkAction(conn, servicecontrol.UnitFileActionEnable, true, true, []string{in.ServiceName})
 		if err != nil {
-
 			log.Println("failed to enable service")
-			out = "failed unit file action"
+			out = []byte("failed unit file action")
 		}
-
-	} else if in.GetUnitFileAction() == services.ServiceActionRequest_UNIT_FILE_ACTION_DISABLE {
-
+	} else if in.GetUnitFileAction() == v2.ServiceActionRequest_UNIT_FILE_ACTION_DISABLE {
 		err = manager.RunSymlinkAction(conn, servicecontrol.UnitFileActionDisable, true, true, []string{in.ServiceName})
 		if err != nil {
-
 			log.Println("failed to disable service")
-			out = "failed unit file action"
+			out = []byte("failed unit file action")
 		}
-
 	} else {
-
 		log.Println("failed to do everything")
-		out = "failed alot"
-
+		out = []byte("failed alot")
 	}
 
-	if in.GetUnitAction() == services.ServiceActionRequest_UNIT_ACTION_START {
-
+	if in.GetUnitAction() == v2.ServiceActionRequest_UNIT_ACTION_START {
 		err = manager.RunAction(conn, servicecontrol.UnitActionStart, in.ServiceName)
 		if err != nil {
-
 			log.Println("failed to run action")
-			out = "failed unit action"
+			out = []byte("failed unit action")
 		}
-
-	} else if in.GetUnitAction() == services.ServiceActionRequest_UNIT_ACTION_STOP {
-
+	} else if in.GetUnitAction() == v2.ServiceActionRequest_UNIT_ACTION_STOP {
 		err = manager.RunAction(conn, servicecontrol.UnitActionStop, in.ServiceName)
 		if err != nil {
-
 			log.Println("failed to run action")
-			out = "failed unit action"
+			out = []byte("failed unit action")
 		}
-
-	} else if in.GetUnitAction() == services.ServiceActionRequest_UNIT_ACTION_RESTART {
-
+	} else if in.GetUnitAction() == v2.ServiceActionRequest_UNIT_ACTION_RESTART {
 		err = manager.RunAction(conn, servicecontrol.UnitActionRestart, in.ServiceName)
 		if err != nil {
-
 			log.Println("failed to run action")
-			out = "failed unit action"
+			out = []byte("failed unit action")
 		}
-
 	} else {
-
 		log.Println("failed to to do everything")
-		out = "failed a lot"
+		out = []byte("failed a lot")
 	}
 
 	err = manager.RunRetrieval(conn, true)
 	if err != nil {
 		log.Println("failed to do everything")
-		out = "failed even more"
+		out = []byte("failed even more")
 	}
 
-	return &services.ServiceActionReply{Status: out}, nil
+	return &v2.ServiceActionReply{Status: out}, nil
 }
