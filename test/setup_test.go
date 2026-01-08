8dfd98fd21a8fef3886e06b1bd2691b473cb11b6
nasr <nsrddyn@gmail.com>
2026-01-08 23:40:57 +0100

    refactor: added the new services and resources version

--- test/setup_test.go
+++ test/setup_test.go
@@ -1,17 +1,23 @@
 package server_test
 
 
+/**
+	
+TODO(nasr): update tests for the new grpc handler services version
+*/
 import (
 	"context"
 	"net"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/test/bufconn"
-	"paradigm-ehb/agent/pkg/grpc_handler"
-	respb "paradigm-ehb/agent/gen/resources/v1"
+	"paradigm-ehb/agent/pkg/grpchandler"
+	servicesHandlerV1 "paradigm-ehb/agent/pkg/grpchandler/services/v1"
+	servicesHandlerV2 "paradigm-ehb/agent/pkg/grpchandler/services/v2"
+	// respb "paradigm-ehb/agent/gen/resources/v1"
+
 	serpb_v1 "paradigm-ehb/agent/gen/services/v1"
 	serpb_v2 "paradigm-ehb/agent/gen/services/v2"
-
 	greetpb "paradigm-ehb/agent/gen/greet"
 	journalpb "paradigm-ehb/agent/gen/journal/v1"
 
@@ -37,9 +43,9 @@ func init() {
 
 	grpc_health_v1.RegisterHealthServer(server, healthServer)
 
-	respb.RegisterResourcesServiceServer(server, &grpc_handler.ResourcesService{})
-	serpb_v1.RegisterHandlerServiceServer(server, &grpc_handler.HandlerService{})
-	serpb_v2.RegisterHandlerServiceServer(server, &grpc_handler.HandlerServiceV2{})
+	// respb.RegisterResourcesServiceServer(server, &grpc_handler.ResourcesService{})
+	serpb_v1.RegisterHandlerServiceServer(server, &servicesHandlerV1.HandlerService{})
+	serpb_v2.RegisterHandlerServiceServer(server, &servicesHandlerV2.HandlerServiceV2{})
 	greetpb.RegisterGreeterServer(server, &grpc_handler.GreeterServer{})
 	journalpb.RegisterJournalServiceServer(server, &grpc_handler.JournalService{})
 
